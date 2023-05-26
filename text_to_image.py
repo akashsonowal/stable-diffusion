d5f87e3de9fbfad8911e0f40814aaaa4d461cf9f
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-26 18:20:46 +0530

    Update text_to_image.py

--- text_to_image.py
+++ text_to_image.py
@@ -5,3 +5,6 @@ from pathlib import Path
 import torch
 
 from stable_diffusion.latent_diffusion import LatentDiffusion
+from stable_diffusion.sampler.ddim import DDIMSampler
+from stable_diffusion.sampler.ddpm import DDPMSampler
+from stable_diffusion.util import load_model, save_images, set_seed
