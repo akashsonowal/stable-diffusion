18cf7fbd07bedcd4fbf7ff60f8a279223de25cba
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-27 22:35:10 +0530

    Update ddim.py

--- stable_diffusion/sampler/ddim.py
+++ stable_diffusion/sampler/ddim.py
@@ -1,1 +1,10 @@
+from typing import Optional, List
 
+import numpy as np
+import torch
+
+from stable_diffusion.latent_diffusion import LatentDiffusion
+import DiffusionSampler
+
+class DDIMSampler(DiffusionSampler):
+  pass
