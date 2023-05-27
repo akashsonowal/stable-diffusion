9f6935e955a84d094fd3cd55404dcac4620d3a09
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-27 08:33:53 +0530

    Update latent_diffusion.py

--- stable_diffusion/latent_diffusion.py
+++ stable_diffusion/latent_diffusion.py
@@ -2,3 +2,7 @@ from typing import List
 
 import torch
 import torch.nn as nn
+
+from stable_diffusion.models.autoencoder import Autoencoder
+from stable_diffusion.models.clip_embedder import CLIPTextEmbedder
+from stable_diffusion.models.unet import UNetModel
