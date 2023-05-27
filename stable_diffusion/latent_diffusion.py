e599eea37f705ec4cf857d35417ecf30dbe306f3
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-27 09:30:42 +0530

    Update latent_diffusion.py

--- stable_diffusion/latent_diffusion.py
+++ stable_diffusion/latent_diffusion.py
@@ -20,7 +20,9 @@ class LatentDiffusion(nn.Module):
   first_stage_model: Autoencoder
   cond_stage_model: CLIPTextEmbedder
   
-  def __init__(self, unet_model: UNetModel):
+  def __init__(self, unet_model: UNetModel, autoencoder: Autoencoder, clip_embedder: CLIPTextEmbedder, latent_scaling_factor: float, n_steps: int, linear_start: float, linear_end: float):
+    super().__init__()
+    self.first_stage_model = autoencoder
     self.model = DiffusionWrapper(unet_model)
   
   def forward(self, x: torch.Tensor, t: torch.Tensor, context: torch.Tensor):
