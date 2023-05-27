d4c871b33b38925f07a248389db1f8c2358d0156
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-27 08:38:11 +0530

    Update latent_diffusion.py

--- stable_diffusion/latent_diffusion.py
+++ stable_diffusion/latent_diffusion.py
@@ -6,3 +6,11 @@ import torch.nn as nn
 from stable_diffusion.models.autoencoder import Autoencoder
 from stable_diffusion.models.clip_embedder import CLIPTextEmbedder
 from stable_diffusion.models.unet import UNetModel
+
+class DiffusionModule(nn.Module):
+  def __init__(self, diffusion_model: UNetModel):
+    super().__init__()
+    self.diffusion_model = diffusion_model
+  
+  def forward(self):
+    pass
