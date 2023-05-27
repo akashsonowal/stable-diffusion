6a8820d455c02149410d738ade30dcae30c3a14f
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-27 09:06:14 +0530

    Update latent_diffusion.py

--- stable_diffusion/latent_diffusion.py
+++ stable_diffusion/latent_diffusion.py
@@ -12,5 +12,17 @@ class DiffusionWrapper(nn.Module):
     super().__init__()
     self.diffusion_model = diffusion_model
   
-  def forward(self):
-    pass
+  def forward(self, x: torch.Tensor, time_steps: torch.Tensor, context: torch.Tensor):
+    return self.diffusion_model(x, time_steps, context)
+ 
+class LatentDiffusion(nn.Module):
+  model: DiffusionWrapper
+  first_stage_model: Autoencoder
+  cond_stage_model: CLIPTextEmbedder
+  
+  def __init__(self, unet_model: UNetModel):
+    self.model = DiffusionWrapper(unet_model)
+  
+  def forward(self, x: torch.Tensor, t: torch.Tensor, context: torch.Tensor):
+    """Predict noise given the latent representations x, time step t and conditioning context c"""
+    return self.model(x, t, context)
