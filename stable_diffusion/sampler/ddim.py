83a5ee4a88171e8efdb505e1b3afda7327da0384
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-27 22:48:20 +0530

    Update ddim.py

--- stable_diffusion/sampler/ddim.py
+++ stable_diffusion/sampler/ddim.py
@@ -7,4 +7,8 @@ from stable_diffusion.latent_diffusion import LatentDiffusion
 import DiffusionSampler
 
 class DDIMSampler(DiffusionSampler):
+  model: LatentDiffusion
+  
+  def __init__(self, model: LatentDiffusion. n_steps: int, ddim_discretize: str = "uniform", ddim_eta: float = 0.):
+    super().__init__(model)
   pass
