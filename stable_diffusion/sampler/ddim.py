b6daf2174c3f870fe5d6484a0ccc210c28f05471
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-28 09:35:42 +0530

    Update ddim.py

--- stable_diffusion/sampler/ddim.py
+++ stable_diffusion/sampler/ddim.py
@@ -11,4 +11,12 @@ class DDIMSampler(DiffusionSampler):
   
   def __init__(self, model: LatentDiffusion. n_steps: int, ddim_discretize: str = "uniform", ddim_eta: float = 0.):
     super().__init__(model)
-  pass
+    self.n_steps = model.n_steps
+    
+    if ddim_discretize == "uniform":
+      c = self.n_steps // n_steps
+      self.time_steps = np.asarray(list(range(0, self.n_steps, c))) + 1
+    elif ddim_discretize == "quad":
+      self.time_steps = ((np.linspace(0, np.sqrt(self.n_steps * .8), n_steps))**2).astype(int) + 1
+    else:
+      raise NotImplementedError(ddim_discretize)
