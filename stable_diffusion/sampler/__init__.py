e5a61e78760b9b2fbe56546d94f047cb9f1702ff
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-28 12:23:11 +0530

    Update __init__.py

--- stable_diffusion/sampler/__init__.py
+++ stable_diffusion/sampler/__init__.py
@@ -1,1 +1,10 @@
+from typing import Optional, List
+import torch
+from stable_diffusion.latent_diffusion import LatentDiffusion
 
+class DiffusionSampler:
+  model: LatentDiffusion
+  def __init__(self, model: LatentDiffusion):
+    super().__init__()
+    self.model = model
+    self.n_steps = model.n_steps
