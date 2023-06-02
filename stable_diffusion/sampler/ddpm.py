97acad9af1561977beee54bf210f469456340fdc
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-02 21:36:09 +0530

    Update ddpm.py

--- stable_diffusion/sampler/ddpm.py
+++ stable_diffusion/sampler/ddpm.py
@@ -5,3 +5,8 @@ import torch.nn.functional as F
 import torch.utils.data
 from torch import nn
 
+from stable_diffusion.util import gather
+
+class DenoiseDiffusion:
+  def __init__(self, eps_model: nn.Module, n_steps: int, device: torch.device):
+    super().__init__()
