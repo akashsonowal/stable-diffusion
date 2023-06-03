43f390d0b52db5d742479e4e065138df98ea12e2
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-03 12:28:21 +0530

    update

--- stable_diffusion/sampler/ddpm.py
+++ stable_diffusion/sampler/ddpm.py
@@ -19,6 +19,23 @@ class DDPMSampler(DiffusionSampler):
       self.sqrt_alpha_bar = alpha_bar ** .5
       self.sqrt_1m_alpha_bar = (1. - alpha_bar) ** .5
       self.recip_alpha_bar = alpha_bar ** -.5
+      self.recip_m1_alpha_bar = (1 / alpha_bar - 1) ** .5
+      variance = beta * (1. - alpha_bar_prev) / (1. - alpha_bar)
+      self.log_var = torch.log(torch.clamp(variance, min=1e-20)) 
+      self.mean_x0_coeff = beta * (alpha_bar_prev ** .5) / (1. - alpha_bar)
+      self.mean_xt_coeff = (1. - alpha_bar_prev) * (1 - beta) ** 0.5 / (1. - alpha_bar)
+  
+  @torch.no_grad()
+  def sample(self, shape: List[int], cond: torch.Tensor, repeat_noise: bool = False, temperature: float = 1., x_last: Optional[torch.Tensor] = None, uncond_scale: float = 1., uncond_cond: Optional[torch.Tensor] = None, skip_steps: int = 0):
+    device = self.model.device
+    bs = shape[0]
+    x = x_last if x_last is not None else torch.randn(shape, device=device)
+    time_steps = np.flip(self.time_steps)[skip_steps:]
+    
+    for step in time_steps:
+      ts = x.new_full((bs,), step, dtype=torch.long)
+      x, pred_x0, e_t = self.p_sample(x, cond, ts, step, repeat_noise=repeat_noise, temperature=temperature, uncond_scale=uncond_scale, uncond_cond=uncond_cond)
+      return x
   
   @torch.no_grad()
   def p_sample(self):
