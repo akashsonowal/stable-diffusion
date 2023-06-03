296c2aa9418bbc728fc2d7ce18df858e4058551e
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-03 07:33:02 +0530

    Update ddpm.py

--- stable_diffusion/sampler/ddpm.py
+++ stable_diffusion/sampler/ddpm.py
@@ -1,12 +1,10 @@
 from typing import Tuple, Optional
 
+import numpy as np
 import torch
-import torch.nn.functional as F
-import torch.utils.data
-from torch import nn
 
-from stable_diffusion.util import gather
+from stable_diffusion.latent_diffusion import LatentDiffusion
+from stable_diffusion.sampler import DiffusionSampler
 
-class DenoiseDiffusion:
-  def __init__(self, eps_model: nn.Module, n_steps: int, device: torch.device):
-    super().__init__()
+class DDPMSampler(DiffusionSampler):
+  model: LatentDiffusion
