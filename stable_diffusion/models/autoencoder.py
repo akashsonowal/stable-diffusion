dba0ca80cff7aa02f8bb004ecb42b6f2cbff3ecd
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-04 11:18:08 +0530

    update

--- stable_diffusion/models/autoencoder.py
+++ stable_diffusion/models/autoencoder.py
@@ -14,14 +14,35 @@ class AutoEncoder(nn.Module):
         self.quant_conv = nn.Conv2d(2*z_channels, 2*emb_channels, 1) # both mean and variance
         self.post_quant_conv = nn.Conv2d(emb_channels, z_channels, 1)
     
-    def encode():
-        pass
+    def encode(self, img: torch.Tensor) -> "GaussianDistribution":
+        z = self.encoder(img) # (batch_size, z_channels * 2, height, weight)
+        moments = self.quant_conv(z)
+        return GaussianDistribution(moments)
     
     def decode():
         pass
     
 class Encoder(nn.Module):
-    pass 
+    def __init__(self, *, channels: int, channels_multipliers: List[int], n_resnet_blocks: int, in_channels: int, z_channels: int):
+        super().__init__()
+        n_resolutions = len(channels_multipliers)
+        self.conv_in = nn.Conv2d(in_channels, channels, 3, stride=1, padding=1)
+        channels_list = [m * channels for m in [1] + channels_multipliers]
+
+        self.down = nn.ModuleList()
+
+        for i in range(n_resolutions):
+            resnet_blocks = nn.ModuleList()
+
+            for _ in range(n_resnet_blocks):
+                resnet_blocks.append(ResNetBlock(channels, channels_list[i + 1]))
+                channels = channels_list[i + 1]
+
+        self.conv_out = nn.Conv2d(channels, 2*z_channels, 3, stride=1, padding=1)
+    
+    def forward(self, img: torch.Tensor):
+        x = self.conv_out(x)
+        return x
 
 class Decoder(nn.Module):
     pass 
