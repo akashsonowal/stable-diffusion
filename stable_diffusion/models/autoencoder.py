1e5da4f2f8d3ccbe9b057fc75f08bba05df34aa7
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-04 10:20:27 +0530

    update

--- stable_diffusion/models/autoencoder.py
+++ stable_diffusion/models/autoencoder.py
@@ -5,4 +5,11 @@ import torch.nn.functional as F
 from torch import nn
 
 class AutoEncoder(nn.Module):
-    
+    def __init__(self, encoder: Encoder, decoder: Decoder, emb_channels: int, z_channels: int):
+        # embedding_space (z) to quantized_embedding_space (emb)
+        super().__init__()
+        self.encoder = encoder 
+        self.decoder = decoder 
+        
+
+
