15ead671e6c505ef2d876ccd9776d2233d791502
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-08 08:09:31 +0530

    update

--- stable_diffusion/models/unet_attention.py
+++ stable_diffusion/models/unet_attention.py
@@ -5,7 +5,8 @@ import torch.nn.functional as F
 from torch import nn
 
 class SpatialTransformer(nn.Module):
-    pass 
+    def __init__(self, channels: int, n_heads: int, n_layers: int, d_cond: int):
+        super().__init__()
 
 class BasicTransformerBlock(nn.Module):
     pass 
