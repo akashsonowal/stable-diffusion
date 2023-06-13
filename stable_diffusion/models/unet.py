5d2d0874d8cfe3af6247c10566d0669cb8eab258
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-13 23:20:07 +0530

    update

--- stable_diffusion/models/unet.py
+++ stable_diffusion/models/unet.py
@@ -35,8 +35,9 @@ class DownSample(nn.Module):
 class ResBlock(nn.Module):
     pass 
 
-class GroupNorm32():
-    pass 
+class GroupNorm32(nn.GroupNorm):
+    def forward(self, x):
+        return super().forward(x.float()).type(x.dtype)
 
 def normalization():
     pass 
