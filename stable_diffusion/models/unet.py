3977a8eccca15c0ba5639c239a6ff35819697734
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-13 23:47:16 +0530

    update

--- stable_diffusion/models/unet.py
+++ stable_diffusion/models/unet.py
@@ -34,9 +34,14 @@ class DownSample(nn.Module):
 
 class ResBlock(nn.Module):
     def __init__(self):
-        self.in_layers = None 
-        self.emb_layers = None 
-        self.out_layers = None 
+        super().__init__()
+
+        if out_channels is None:
+            out_channels = channels 
+        
+        self.in_layers = nn.Sequential(normalization(channels), nn.SiLU(), nn.Conv2d(channels, out_channels, 3, padding=1))
+        self.emb_layers = nn.Sequential(nn.SiLU(), nn.Linear(d_t_emb, out_channels))
+        self.out_layers = nn.Sequential(normalization(out_channels), nn.SiLU(), nn.Dropout(0.), nn.Conv2d(out_channels, out_channels, 3, padding=1))
         if out_channels == channels:
             self.skip_connection = nn.Identity()
         else:
