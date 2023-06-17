0efe7883d37b0f810d8b0d6109e117ab2c1239f9
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-17 17:48:50 +0530

    update

--- stable_diffusion/models/unet_attention.py
+++ stable_diffusion/models/unet_attention.py
@@ -49,4 +49,10 @@ class FeedForward(nn.Module):
     pass 
 
 class GeGLU(nn.Module):
-    pass 
+    def __init__(self, d_in: int, d_out: int):
+        super().__init__()
+        self.proj = nn.Linear(d_in, d_out * 2)
+    
+    def forward(self, x: torch.Tensor):
+        x, gate = self.proj(x).chunk(2, dim=-1)
+        return x * F.gelu(gate)
