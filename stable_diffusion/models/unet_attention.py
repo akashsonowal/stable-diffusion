0d722493cb5e094487e6b8f980bef17cdb924701
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-18 12:17:17 +0530

    update

--- stable_diffusion/models/unet_attention.py
+++ stable_diffusion/models/unet_attention.py
@@ -102,6 +102,20 @@ class CrossAttention(nn.Module):
         return self.to_out(out)
 
     def normal_attention(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+        q = q.view(*q.shape[:2], self.n_heads, -1) # [batch_size, seq_len, n_heads, d_head]
+        k = k.view(*k.shape[:2], self.n_heads, -1)
+        v = v.view(*v.shape[:2], self.n_heads, -1)
+        attn = torch.einsum('bhij,bjhd -> bihd', q, k) * self.scale
+
+        if self.is_inplace:
+            half = attn.shape[0] // 2
+            attn[half:] = attn[half:].softmax(dim=-1)
+            attn[:half] = attn[:half].softmax(dim=-1)
+        else:
+            attn = attn.softmax(dim=-1)
+        
+        out = torch.einsum('bhij, bjhd -> bihd', attn, v)
+        out = out.reshape(*out.shape[:2], -1)
         return self.to_out(out)
 
 class FeedForward(nn.Module):
