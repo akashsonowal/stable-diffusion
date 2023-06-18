7140289ad9e65d8789ee807f4441a10af5c51032
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-18 10:29:43 +0530

    update

--- stable_diffusion/models/unet_attention.py
+++ stable_diffusion/models/unet_attention.py
@@ -83,8 +83,22 @@ class CrossAttention(nn.Module):
         batch_size, seq_len, _ = q.shape 
         qkv = torch.stack((q, k, v), dim=2) # [batch_size, seq_len, 3, n_heads * d_head]
         qkv = qkv.view(batch_size, seq_len, 3, self.n_heads, self.d_head)
-        
 
+        if self.d_head <= 32:
+            pad = 32 - self.d_head 
+        elif self.d_head <= 64:
+            pad = 64 - self.d_head
+        elif self.d_head <= 128:
+            pad = 128 - self.d_head
+        else:
+            raise ValueError(f"Head size ${self.head} too large for Flash Attention")
+
+        if pad:
+            qkv = torch.cat((qkv, qkv.new_zeros(batch_size, seq_len, 3, self.n_heads, pad)), dim=-1)
+        
+        out, _ = self.flash(qkv) # [batch_size, seq_len, n_heads, d_padded]
+        out = out[:, :, :, :self.d_head]
+        out = out.reshape(batch_size, seq_len, self.n_heads, self.d_head)
         return self.to_out(out)
 
     def normal_attention(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
